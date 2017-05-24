44f1eeb4442d8fefbcec2e2cab8c7fa55c1e0661
Mário Ferreira <m.j.v.ferreira@hotmail.com>
2017-05-24 20:18:14 +0100

    Add reverseParentheses.py in Arcade/Intro

---
+++ Arcade/Intro/reverseParentheses.py
@@ -0,0 +1,23 @@
+def reverseParentheses(s):
+    a = ""
+    b = ""
+    c = ""
+    flag = 0
+    for i in s:
+        print(flag)
+        if (i=='('):
+            flag += 1
+        if (i==')' and flag==1):
+            flag -= 1
+            a = a+b
+        if (i==')' and flag>1):
+            flag -= 1
+            b = c+b
+        if (flag==0 and (i!='(') and (i!=')')):
+            a = a+i
+        if (flag==1 or flag==3 and (i!='(') and (i!=')')):
+            b = i+b
+        if (flag >1 and (i!='(') and (i!=')')):
+            c = c+i
+    return a
+        
